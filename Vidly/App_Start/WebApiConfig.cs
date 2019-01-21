6e2248b6dff312375e1ef3d229746f81278d757a
Bobby <bobby.inscoe@syncify.se>
2019-01-21 12:27:33 +0100

    Add Mapping functionality to assign Customers to Data Transfer Objects. Create DTO for Customers to pass in the API Controller.

--- Vidly/App_Start/WebApiConfig.cs
+++ Vidly/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Vidly
 {
@@ -9,6 +11,12 @@ namespace Vidly
     {
         public static void Register(HttpConfiguration config)
         {
+            // This is used to format Json calls to conform to normal json (Camel notation) instead of Pascal notation
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    /// <summary>
+    /// Used to pass raw data to calls from the API
+    /// This is used as Data Transfer Objects for our CustomerController API. This is used to prevent security risks when malicious users would attempt to pass data directly to our database.
+    /// DTOs are used to control what properties are passed to the internal controller methods and allows us to fine tune what properties in the entity model can be updated.
+    /// This is also used to simplify a model that is provided to the view.
+    /// </summary>
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        /// <summary>
+        /// Min18YearsIfAMember may need to be removed to prevent a runtime exception for casting to Customer
+        /// </summary>
+        public DateTime? Birthday { get; set; }
+    }
+}
