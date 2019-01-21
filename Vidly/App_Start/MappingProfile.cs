a786acdd887ade18e0e059803f05af2641147ab1
Bobby <bobby.inscoe@syncify.se>
2019-01-21 13:51:30 +0100

    Add full CRUD operations to Customers and Movies API

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -14,6 +14,9 @@ namespace Vidly.App_Start
         {
             Mapper.CreateMap<Customer, CustomerDto>();
             Mapper.CreateMap<CustomerDto, Customer>().ForMember(c => c.Id, opt => opt.Ignore()); // Opt used to ignore customer Id for update requests
+
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MovieDto, Movie>().ForMember(m => m.Id, opt => opt.Ignore());
         }
     }
 }
